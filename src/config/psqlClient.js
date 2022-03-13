23971571cd40ff7f9a344dcb34ae2731413923ff
Maksymilian Andrzejczak <andrzejczakmaks@icloud.com>
2022-03-14 00:14:58 +0100

    fix120

--- src/config/psqlClient.js
+++ src/config/psqlClient.js
@@ -12,7 +12,7 @@ const dbConnData = {
 const devConfig = `postgresql://${process.env.PGUSER || 'postgres'}:${process.env.PGPASSWORD || 'tajne'}@${process.env.PGHOST || '127.0.0.1'}:${process.env.PGPORT || 5432}/${process.env.PGDATABASE}`;
 
 const proConfig = {
-  connectionString: process.env.DATABASE_URL || 'postgres://sdfdsgeynipxqw:1cb1bcb2d9c7f77ba565cf017ca7dcf2ff8dca373fa98dd66d830f4f3554be7a@ec2-52-70-186-184.compute-1.amazonaws.com:5432/dcluuq38blukk', //heroku addons
+  connectionString: process.env.DATABASE_URL
 };
 
 console.log("123", process.env.DATABASE_URL)
